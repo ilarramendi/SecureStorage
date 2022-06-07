459a64f9b4a589ea38dec5ef9aa02b4c244f1c23
ilarramendi <ilarramendi1@gmail.com>
2022-06-07 18:06:52 -0300

    Added ChaCha20-Polly1305 on top of AES-GCM using different keys and nonce, Made keys visible on list, Added option to remove items from storage

--- SecureStorage.py
+++ SecureStorage.py
@@ -1,4 +1,4 @@
-from sys import argv, getsizeof
+from sys import argv
 from random import randint
 from datetime import datetime
 
@@ -18,17 +18,18 @@ if argv[2] == 'encrypt':
     res = ''
     with open(argv[3], 'rb') as f: 
         res = f.read()
-    res = encrypt(res, False)
+    res = encrypt(res)
 
     # Write encrypted data
     with open(fileName + '.ENCRYPTED', 'wb') as f:
         f.write(res[0])
 
-    # Store nonce, padding and name
+    # Store random keys, name and date
     files[fileName] = {
         'name': argv[3].rpartition('/')[2],
         'date': datetime.now().strftime('%m/%d/%Y, %H:%M:%S'),
-        'key': res[1]
+        'keyAES': res[1],
+        'keyChaCha': res[2]
     }
 
     # Encrypt and store files metadata
@@ -47,7 +48,7 @@ elif argv[2] == 'decrypt':
     # Open the file, decrypt it and store it to the new file
     with open(argv[3], 'rb') as f:
         with open(info['name'], 'wb') as f2:
-            f2.write(decrypt(f.read(), info['key'].encode('ISO-8859-1')))
+            f2.write(decrypt(f.read(), info['keyAES'].encode('ISO-8859-1'), info['keyChaCha'].encode('ISO-8859-1')))
         print(f"Successfuly decrypted {argv[3]} as {info['name']}")
 
 elif argv[2] == 'list': 
@@ -55,12 +56,29 @@ elif argv[2] == 'list':
     for name in files:
         length = len(files[name]['name'])
         if length > maxLength: maxLength = length
-
-    print(f"\nName{' ' * (maxLength - 4)} | Encrypted Name       | Date")
-    print(f"-------{'-' * (maxLength - 4)}-------------------------------------------")
+    maxLengthAES = 0
+    for name in files:
+        length = len(str(files[name]['keyAES'].encode('ISO-8859-1')))
+        if length > maxLengthAES: maxLengthAES = length
+    
+    print(f"\nName{' ' * (maxLength - 4)} | Encrypted Name       | Date                 | Key AES {' ' * (maxLengthAES - 7)}| Key Cha Cha        ")
+    print(f"-------{'-' * (20 + maxLength + maxLengthAES * 2 - 4)}")
     for name in files:
         item = files[name]
-        print(f"{item['name']}{' ' * (maxLength - len(item['name']))} | {name}.ENCRYPTED | {item['date']}")
+        keyAES = str(item['keyAES'].encode('ISO-8859-1'))
+        keyChaCha = str(item['keyChaCha'].encode('ISO-8859-1'))
+        print(f"{item['name']}{' ' * (maxLength - len(item['name']))} | {name}.ENCRYPTED | {item['date']} | {keyAES}{' ' * (maxLengthAES - len(keyAES))} | {keyChaCha}")
     print()
+
+elif argv[2] == 'remove': 
+    info = {}
+    name = argv[3].rpartition('.')[0].rpartition('/')[2]
+    if name not in files:
+        print('File not found')
+        exit()
+    
+    del files[name]
+    saveFiles(files, key)
+    print(f"Successfuly removed {argv[3]} from storage")
 
 else: print(f"Wrong action {argv[2]}")
--- functions.py
+++ functions.py
@@ -1,16 +1,23 @@
-import os
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from os import urandom, path
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM, ChaCha20Poly1305
 from cryptography.hazmat.backends import default_backend
 from json import loads, load, dumps, dump
 
-def encrypt(string, key):
-    if not key: key = os.urandom(32)
-    nonce = os.urandom(12)
-    
-    return (nonce + AESGCM(key).encrypt(nonce, string, None), key.decode('ISO-8859-1'))
+def encrypt(string, keyAES = False, keyChaCha = False):
+    if not keyAES: keyAES = urandom(32)
+    if not keyChaCha: keyChaCha = urandom(32)
 
-def decrypt(string, key):
-    return AESGCM(key).decrypt(string[:12], string[12:], None)
+    nonce = urandom(12)
+    nonce2 = urandom(12)
+
+    string = nonce + AESGCM(keyAES).encrypt(nonce, string, None)
+    string = nonce2 + ChaCha20Poly1305(keyChaCha).encrypt(nonce2, string, None)
+    
+    return (string, keyAES.decode('ISO-8859-1'), keyChaCha.decode('ISO-8859-1'))
+
+def decrypt(string, keyAES, keyChaCha):
+    string = ChaCha20Poly1305(keyChaCha).decrypt(string[:12], string[12:], None)
+    return AESGCM(keyAES).decrypt(string[:12], string[12:], None)
 
 def loadFiles(key):
     try:
@@ -18,14 +25,14 @@ def loadFiles(key):
         with open('files.ENCRYPTED', 'rb') as f: data = f.read()
 
         # Decrypt files.ENCRYPTED and load as a json
-        return loads(decrypt(data, key), encoding='ISO-8859-1')
+        return loads(decrypt(data, key, key), encoding='ISO-8859-1')
     except:
-        if os.path.exists('files.ENCRYPTED'):
+        if path.exists('files.ENCRYPTED'):
             print('Error loading files.ENCRYPTED')
             exit()
         else: return {}
 
 def saveFiles(files, key):
-    res = encrypt(str.encode(dumps(files, ensure_ascii=False)), key)
+    res = encrypt(str.encode(dumps(files, ensure_ascii=False)), key, key)
     with open('files.ENCRYPTED', 'wb') as f:
         f.write(res[0])
